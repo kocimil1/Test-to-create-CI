aac6096fa173eb910924006778604bd1f4f6e2d8
Milan Koci <kocimil1@fjfi.cvut.cz>
2024-12-03 12:39:25 +0100

    Change testing library to Google Test

--- src/add.h
+++ src/add.h
@@ -2,5 +2,6 @@
 #define ADD_H
 
 int add(int a, int b);
+double add(double a, double b);
 
 #endif
--- tests/test_add.cpp
+++ tests/test_add.cpp
@@ -1,12 +1,31 @@
-#include "add.h"
-#include <cassert>
-#include <iostream>
+// #include "add.h"
+#include <gtest/gtest.h>
 
-int main() {
-    assert(add(2, 3) == 5);
-    assert(add(-1, 1) == 0);
-    assert(add(0, 0) == 0);
+int add(int a, int b) {
+    return a + b;
+}
 
-    std::cout << "All tests passed!" << std::endl;
-    return 0;
+double add(double a, double b) {
+    return a + b;
+}
+
+TEST(AddFunctionTest, HandlesPositiveNumbers) {
+    EXPECT_EQ(add(4, 3), 7);
+}
+
+TEST(AddFunctionTest, HandlesNegativeNumbers) {
+    EXPECT_EQ(add(-1, 1), 0);
+}
+
+TEST(AddFunctionTest, HandlesZero) {
+    EXPECT_EQ(add(0, 0), 5) << "Expected 5, but got " << add(0, 0);
+}
+
+// TEST(AddFunctionTest, HandlesDecimalNumbers) {
+//     EXPECT_EQ(add(2.5, 3.5), 6.1);
+// }
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
